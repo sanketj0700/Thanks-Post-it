c913b90d08fb2e8f802ca460d9fb19164fbef881
karan-bhatia22 <karan.bhatia@searce.com>
2022-02-01 23:43:21 +0530

    Added functionalities and modified design

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
+import {useState} from  'react';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import {Route, Routes, BrowserRouter} from 'react-router-dom';
 import './styles/App.css';
 import SearchBar from './components/SearchBar';
 function App() {
+
+  const [user, setUser] = useState('Karan');
   return (
     <div className="App">
         <Navbar />
-        <Home />
+        <Home user = {user}/>
     </div>
   );
 }
