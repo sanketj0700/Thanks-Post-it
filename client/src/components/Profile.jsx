f8c239a13d883c980cfcbfea319de668488ae042
Dhanvi Shah <dhanvishah@FVFGW159Q05N.local>
2022-02-08 09:16:59 +0530

    Changes in profile page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -4,15 +4,22 @@ import MentionedMe from './MentionedMe';
 import StarredMessages from './StarredMessages';
 import Loading from './Loading';
 import { useAuth0,withAuthenticationRequired } from "@auth0/auth0-react";
+import { Avatar } from '@mui/material';
+import axios from 'axios';
 
 
-export default withAuthenticationRequired( function Profile() {
+export default withAuthenticationRequired( function Profile(props) {
+
+  //const user = props.card.user;
+  //const loggedInUser = props.user;
+  const {user,error,getIdTokenClaims} = useAuth0();
 
   const [section,setSection] = useState(true);
    const toggleSection = (e)=>{
      if(e.target.className === 'messages-1' && !section)
      {
        setSection(true)
+       
      }
      else if(e.target.className === 'messages-2' && section)
      {
@@ -21,34 +28,40 @@ export default withAuthenticationRequired( function Profile() {
    };
 
   return (
-    <div className='profile-page'>
-        <div className='profile-card'>
-            <div className='profile-photo'>
-                <span className='dot'></span>
-            </div>
-            <div className='user-name'>
-                <div className='name'>Dhanvi Shah</div>
-            </div>
-            <div className='profile-badges'>
-              <ul className='diamond'>Diamond badges : </ul>
-              <ul className='golden'>Golden badges : </ul>
-              <ul className='silver'>Silver badges : </ul>
-              <ul className='bronze'>Bronze badges : </ul>
-              <ul className='platinum'>Platinum badges : </ul>
-            </div>
-        </div>
+    <>
+      <div className='profile-page'>
+          <div className='profile-card'>
+              <div className='profile-image'>
+                  <Avatar
+                    alt = {user.name}
+                    src = {user.picture}
+                    sx = {{ width:140, height:140 }}
+                  />
+              </div>
+              <div className='user-name'>
+                  <div className='name'>{user.name} </div>
+              </div>
+              <div className='profile-badges'>
+                <ul className='diamond'>Diamond badges : </ul>
+                <ul className='golden'>Golden badges : </ul>
+                <ul className='silver'>Silver badges : </ul>
+                <ul className='bronze'>Bronze badges : </ul>
+                <ul className='platinum'>Platinum badges : </ul>
+              </div>
+          </div>
 
-        <div className='messages-panel'>
-            <p className='messages-1' onClick={toggleSection}>Starred Messages</p>
-            <p className='messages-2' onClick={toggleSection}>Mentioned Me</p>
-        </div>
+          <div className='messages-panel'>
+              <p className='messages-1' onClick={toggleSection}>Starred Messages</p>
+              <p className='messages-2' onClick={toggleSection}>Mentioned Me</p>
+          </div>
 
-        <div className='messages-list'>
-          {
-            section ? <StarredMessages /> : <MentionedMe />
-          }
-        </div>
-    </div>
+          <div className='messages-list'>
+            {
+              section ? <StarredMessages /> : <MentionedMe />
+            }
+          </div>
+      </div>
+    </>
   );
 }, {
   onRedirecting: () => (<Loading />)
